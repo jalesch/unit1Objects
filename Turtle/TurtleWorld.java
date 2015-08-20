c71c6d4a982d0558312fc19a3ba67f5586a761ba
jalesch <johnalesch@gmail.com>
2015-08-20 10:29:26 -0500

    Made the turtle move, added pen Width

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -6,8 +6,14 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        turtle.setPenColor(Color.BLUE);
         turtle.penDown();
+        turtle.forward(50);
+        turtle.penUp();
+        turtle.forward(100);
+        turtle.setPenWidth(10);
+        turtle.penDown();
+        turtle.turnRight();
         turtle.forward(50);
     }
 }
