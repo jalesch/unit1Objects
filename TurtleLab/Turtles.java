f7b0e0dd249cf4e7c7fa440566318006ec707873
jalesch <johnalesch@gmail.com>
2015-09-03 10:24:05 -0500

    Finished Lab

--- TurtleLab/Turtles.java
+++ TurtleLab/Turtles.java
@@ -1,6 +1,6 @@
 import java.awt.Color;
 import java.util.Random;
-
+//This is what my two year old would draw :)
 public class Turtles
 {
     public static void main( String[] args )
@@ -10,21 +10,25 @@ public class Turtles
         Turtle t2 = new Turtle(turtleWorld);
         t1.setPenColor(Color.BLUE);
         t2.setPenColor(Color.ORANGE);
-        t1.penDown();
-        t2.penDown();
+        t1.hide();
+        t2.hide();
+        t1.penUp();
+        t2.penUp();
         t1.forward(100);
         t2.turnRight();
         t2.forward(100);
+        t1.penDown();
+        t2.penDown();
         Random generator = new Random();
-        for(int i=1; i<100; i++){
-            int turn1 = generator.nextInt(360);
-            int turn2 = generator.nextInt(360);
-            int d1 = generator.nextInt(200);
-            int d2 = generator.nextInt(200);
+  
+
+        for(int i=1; i<360; i++){
+            int turn1 = 10;
+            int d1 = 10;
             t1.turn(turn1);
-            t2.turn(turn2);
+            t2.turn(turn1);
             t1.forward(d1);
-            t2.forward(d2);
+            t2.forward(d1);
          }
     }
 }
